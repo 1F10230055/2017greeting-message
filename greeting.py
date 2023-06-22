576c6441927016fd366353d363abdfc9f7a64e8f
1F10230055 <s1F102300559@iniad.org>
2023-06-22 15:06:33 +0900

    fixed

--- greeting.py
+++ greeting.py
@@ -3,11 +3,11 @@ from datetime import datetime
 def greet(name):
     hour = datetime.now().hour
     if hour <= 11:
-        message = 'Good morning' + name + '-san!'
+        message = 'Good morning, ' + name + '-san!'
     elif hour <= 17:
         message = 'Hello, ' + name + '-san!'
     else:
-        message = 'Good evening' + name + '-san!'
+        message = 'Good evening, ' + name + '-san!'
     print(message)
 
 
